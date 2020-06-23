26d1285690bca023646d168644adc60ee3f21004
Akshay-ch-dj <akshaych203@gmail.com>
2020-06-23 20:25:30 +0530

    Added searching filter to profiles_api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,13 @@
-from rest_framework.views import APIView # REST API Views(Django REST framework)
-# All API views expected to return a std. Response
-from rest_framework.response import Response
-# Status object contains handy HTTP status codes, one can use when returning responses
-from rest_framework import status
-# Importing viewsets
-from rest_framework import viewsets
+from rest_framework import viewsets # Importing viewsets
+# from rest_framework.views import APIView # REST API Views(Django REST framework)
+# # All API views expected to return a std. Response
+# from rest_framework.response import Response
+# # Status object contains handy HTTP status codes, one can use when returning responses
+# from rest_framework import status
 # To authenticate users with the API,
 from rest_framework.authentication import TokenAuthentication
+# To add search functionality
+from rest_framework import filters
 
 from . import serializers
 from profiles_api import models
@@ -29,3 +30,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,) # To create as Tuple
     # Permission classes(how the user gets permission to do certain things)
     permission_classes = (permissions.UpdateOwnProfile,)
+    # The rest_framework comes out of the box with some filters that can be added
+    # filtering any viewsets, (one or more filters can be added)
+    filter_backends = (filters.SearchFilter,)
+    # To tell the filtes which fields needs to be searched
+    search_fields = ('name', 'email',)
